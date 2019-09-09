e57144757066dd1871d72c862b337a11258e68bd
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-09 20:06:09 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
+    # @post.update(post_params)
+    
     redirect_to post_path(@post)
   end
 
